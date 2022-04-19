09a1992b4a329217b5a6dee3a6a9c96c531f4ec7
a145789 <2091927351@qq.com>
2022-04-19 19:55:07 +0800

    feat: App

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,25 @@ const { isShowLevel } = $(useStore());
   <div class="h-full w-full flex justify-center">
     <div class="m-42px mt-110px">
       <h2 class="text-center text-#229453">华容道</h2>
-      <Level v-if="isShowLevel" />
-      <Game v-else />
+      <Transition appear name="fade" mode="out-in">
+        <Level v-if="isShowLevel" />
+        <Game v-else />
+      </Transition>
     </div>
   </div>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
 
 <style>
 html,
