10d3b2fd693a6ddfedd4b74d692cf750abfa27e9
clencat <37403253+a145789@users.noreply.github.com>
2022-04-20 11:52:53 +0800

    Update Level.vue

--- src/components/Level.vue
+++ src/components/Level.vue
@@ -13,7 +13,7 @@ function setLevel(index: number) {
 <template>
   <div class="flex flex-wrap">
     <div
-      class="cursor-pointer underline mr-6 mb-3 text-#5d655f text-18px"
+      class="cursor-pointer underline mr-6 mb-3 text-#5d655f dark:text-#d1d5db text-18px"
       v-for="({ name }, index) in gameConfig"
       :key="name"
       @click="setLevel(index)"
