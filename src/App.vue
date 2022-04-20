c35f93926f11201063ed16ac39bad35656cc3310
a145789 <2091927351@qq.com>
2022-04-20 15:07:57 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,54 @@
 <script setup lang="ts">
 import Game from "./components/Game.vue";
 import Level from "./components/Level.vue";
-import { WeatherSunny16Regular, WeatherMoon28Filled } from "@vicons/fluent";
+import WeatherSunny16Regular from "@vicons/fluent/WeatherSunny16Regular";
+import WeatherMoon28Filled from "@vicons/fluent/WeatherMoon28Filled";
 import { Icon } from "@vicons/utils";
 
 import { useStore } from "@/store";
+import { watchEffect } from "vue";
 
 const { isShowLevel } = $(useStore());
-let isDark = $ref(false);
+let isDark = $ref(Boolean(localStorage.getItem("huarongdao_dark")));
+watchEffect(() => {
+  document.body.classList.toggle("dark", isDark);
+  localStorage.setItem("huarongdao_dark", isDark ? "true" : "");
+});
 </script>
 
 <template>
-  <div class="h-full w-full" :class="{ dark: isDark }">
-    <div
-      class="h-full w-full flex justify-center relative bg-#9abeaf dark:bg-#141e1b transition duration-700"
-    >
-      <div class="m-42px mt-110px">
-        <h2 class="text-center text-#229453 dark:text-#d17525">华容道</h2>
-        <Transition appear name="fade" mode="out-in">
-          <Level v-if="isShowLevel" />
-          <Game v-else />
-        </Transition>
-      </div>
-
-      <Icon
-        class="absolute top-10 right-10"
-        size="36"
-        :color="isDark ? '#fff' : '#ddd '"
-        @click="isDark = !isDark"
-      >
-        <Transition name="fade" mode="out-in">
-          <WeatherMoon28Filled v-if="!isDark" />
-          <WeatherSunny16Regular v-else="isDark" />
-        </Transition>
-      </Icon>
+  <div
+    class="h-full w-full flex justify-center relative bg-#9abeaf dark:bg-#141e1b transition duration-700"
+  >
+    <div class="m-42px mt-110px">
+      <h2 class="text-center text-#229453 dark:text-#d17525">华容道</h2>
+      <Transition appear name="fade" mode="out-in">
+        <Level v-if="isShowLevel" />
+        <Game v-else />
+      </Transition>
     </div>
+
+    <Icon
+      class="absolute top-10 right-10"
+      size="36"
+      :color="isDark ? '#fff' : '#ddd '"
+      @click="isDark = !isDark"
+    >
+      <Transition name="fade" mode="out-in">
+        <WeatherMoon28Filled v-if="!isDark" />
+        <WeatherSunny16Regular v-else="isDark" />
+      </Transition>
+    </Icon>
   </div>
 </template>
 
-<style scoped>
+<style>
+html,
+body,
+#app {
+  height: 100%;
+  width: 100%;
+}
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.3s ease;
@@ -47,14 +57,5 @@ let isDark = $ref(false);
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
-}
-</style>
-
-<style>
-html,
-body,
-#app {
-  height: 100%;
-  width: 100%;
 }
 </style>
